9d69d4dfe0d247bcdc7deb009d4679412c37678a
Mahadi Hasan <87907980+mahadi-xlr8@users.noreply.github.com>
2023-10-24 13:13:52 +0600

    initial commit for version 1

---
+++ index.js
@@ -0,0 +1,37 @@
+const ModalWrapper = ({ children, isOpen, onClose, backgroundColor, position }) => {
+    const positionValue = position || {
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%,-50%)",
+    };
+    return isOpen ? (
+      <>
+        <div
+          style={{
+            backgroundColor: backgroundColor || "rgb(1,1,1,0.6)",
+            position: "fixed",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            zIndex: 10000,
+          }}
+          onClick={onClose}
+          id="modal-background"
+        ></div>
+        <div
+          style={{
+            ...positionValue,
+            position: "fixed",
+            zIndex: 10000,
+          }}
+          id="modal-content"
+        >
+          {children}
+        </div>
+      </>
+    ) : null;
+  };
+  
+  export default ModalWrapper;
+  
